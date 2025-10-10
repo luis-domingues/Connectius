592f7caab98c7c68c35fc16eaba6022a1e584053
luis-domingues <luisdomingues117@gmail.com>
2025-10-10 15:02:30 -0300

    chore: reorganize project structure to conventional .NET layout

---
+++ tests/Connectius.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Connectius.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/Connectius.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Connectius.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
