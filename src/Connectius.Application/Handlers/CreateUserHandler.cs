71f8c95dfdfabbb5740257fb2be169c6b7a1ae00
luis-domingues <luisdomingues117@gmail.com>
2025-10-17 16:23:50 -0300

    feat(application): implement interface to obtain hash for saving in the repository

--- src/Connectius.Application/Handlers/CreateUserHandler.cs
+++ src/Connectius.Application/Handlers/CreateUserHandler.cs
@@ -1,4 +1,5 @@
 using Connectius.Application.DTOs;
+using Connectius.Application.Interfaces;
 using Connectius.Domain.Entities;
 using Connectius.Domain.Interfaces;
 
@@ -7,19 +8,22 @@ namespace Connectius.Application.Handlers;
 public class CreateUserHandler
 {
     private readonly IUserRepository _userRepository;
+    private readonly IPasswordHasher _passwordHasher;
 
-    public CreateUserHandler(IUserRepository userRepository)
+    public CreateUserHandler(IUserRepository userRepository, IPasswordHasher passwordHasher)
     {
         _userRepository = userRepository;
+        _passwordHasher = passwordHasher;
     }
 
     public async Task HandleAsync(UserDto userDto)
     {
+        string passwordHash = _passwordHasher.HashPassword(userDto.PasswordHash);
         var newUser = new User(
             userDto.DisplayName,
             userDto.Username,
             userDto.Email,
-            userDto.PasswordHash
+            passwordHash
             );
 
         await _userRepository.AddAsync(newUser);
