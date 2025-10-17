4cbea3ee4a001a70ccba38bc4353fb61797a11da
WillyamPinheiro <marketing@trendsautomation.com.br>
2025-10-17 13:30:39 -0300

    feat(webui): configure dependency injection and SQLite persistence

--- src/Connectius.WebUI/Program.cs
+++ src/Connectius.WebUI/Program.cs
@@ -1,41 +1,35 @@
+using Connectius.Application.Handlers;
+using Connectius.Domain.Interfaces;
+using Connectius.Infrastructure.Data;
+using Connectius.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddDbContext<UserDbContext>(options => options
+    .UseSqlite(builder.Configuration
+        .GetConnectionString("UserConnection")));
+
+builder.Services.AddScoped<IUserRepository,UserRepository>();
+
+builder.Services.AddControllers();
+builder.Services.AddScoped<CreateUserHandler>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
